51688a29f0b10c6a97845dfd62d0c214430e0a63
aashana94 <aashananijhawan@gmail.com>
2018-09-14 16:47:18 +0200

    checking package for final errors and build package

--- tests/testthat/test_euclidean.R
+++ tests/testthat/test_euclidean.R
@@ -1,16 +1,14 @@
 context("euclidean")
 
-<<<<<<< HEAD
+
 test_that("GDC is calculated correctly.", {
   expect_equal(euclidean(123612, 13892347912), 4)
   expect_equal(euclidean(100, 1000), 100)
   expect_equal(euclidean(-100, 1000), 100)
 })
 
-=======
->>>>>>> 9e5d4dc93cb31b3b2b1a1e6a2a2d3f8dd672cd15
 test_that("Wrong input throws an error.", {
-  expect_error(euclidean("100", 1000))
+  expect_error(euclidean("100", 1000))  
   expect_error(euclidean(100, "1000"))
-  expect_error(euclidean(TRUE, "1000"))
+  expect_error(euclidean(TRUE, "1000"))  
 })
